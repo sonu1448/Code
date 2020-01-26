616775ec63cdd59412e5d955a978461f3187841d
sonu1448 <damoliaarvind@gmail.com>
2020-01-26 23:55:39 +0530

    Update OwnWay.cpp

--- codejam2019/OwnWay.cpp
+++ codejam2019/OwnWay.cpp
@@ -12,25 +12,21 @@ int main(){
 		int n;cin>>n;
 		string p;
 		cin>>p;
-		char result[2*n-2];
-		int f=0,temp=0;
-		result[0]=change(p[0]);
-		result[2*n-3]=change(p[2*n-3]);
+		int f=0;
+		p[0]=change(p[0]);
+		p[2*n-3]=change(p[2*n-3]);
 		if(p[0]==p[2*n-3]){
 		    f=1;
 		}
-		cout<<f<<endl;
+		//cout<<f<<endl;
 		for(int i=1;i<(2*n-3);i++){
-		        if(p[i]==p[i+1]  && p[i]!=p[0] && f!=0){
-		        	result[i]=change(p[i]);
-		        	result[i+1]=change(p[i]);
-		        	f=0;
-		        	i++;
+		        if(p[i]==p[i+1]  && p[i]==p[0] && f!=0){
+		        	p[i]=change(p[i]);
+		        	p[i+1]=change(p[i+1]);
+		        	break;
 				}
-				else
-					result[i]=p[i];
 		}
-		cout<<"Case #"<<z<<": "<<result;
+		cout<<"Case #"<<z<<": "<<p;
 		cout<<endl;
 	}
 	return 0;
